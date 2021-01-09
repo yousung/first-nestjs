3e3205a5708f855cf9b65c8198ab245be02aefd7
Yousung Ahn <nug22kr@gmail.com>
2021-01-09 18:51:08 +0900

    controller and service setting

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Todo } from './todos.entity';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(readonly todoService: TodosService) {}
+  @Get('/')
+  index(): Todo[] {
+    return this.todoService.idex();
+  }
+
+  @Get(':id')
+  show(): Todo {
+    return this.todoService.show();
+  }
+
+  @Post()
+  store(): Todo {
+    return this.todoService.store();
+  }
+
+  @Patch(':id')
+  update(): Todo {
+    return this.todoService.update();
+  }
+
+  @Delete(':id')
+  destroy(): null {
+    return this.todoService.delete();
+  }
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './todos.entity';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  readonly todos: Todo[] = [];
+
+  idex(): Todo[] {
+    return this.todos;
+  }
+
+  show(): Todo {
+    return null;
+  }
+
+  store(): Todo {
+    return null;
+  }
+
+  update(): Todo {
+    return null;
+  }
+
+  delete(): null {
+    return null;
+  }
+}
